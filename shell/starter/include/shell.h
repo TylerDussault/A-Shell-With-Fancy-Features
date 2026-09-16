23a6624eea37546960a178f115df11d936c3dd00
mchenrep <matthewchenwork@gmail.com>
2026-09-15 21:33:17 -0400

    added small amount of starter code (REPL, header file, skeleton work)

---
+++ shell/starter/include/shell.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void print_prompt(void);
+
+
--- shell/starter/src/shell.c
+++ shell/starter/src/shell.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "shell.h"
+
+int main()
+{
+    // REPL (read eval print loop)
+    while (1) {
+
+    }
+}
+
+void print_prompt(){
+    
+}
