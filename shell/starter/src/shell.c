656b6426d35da1568f45a9d7d0d2d1b93c69dca2
mchenrep <matthewchenwork@gmail.com>
2026-09-21 21:56:10 -0400

    integrate path search and execute into shell

--- shell/starter/src/shell.c
+++ shell/starter/src/shell.c
@@ -17,9 +17,33 @@ int main()
 
         // tilde expand
         expand_tilde(tokens);
+
+        // if empty input
+        if (tokens->size == 0) {
+            free(input);
+            free_tokens(tokens);
+            continue;
+        }
         
+        // find command in PATH
+        char *fname = find_path(tokens->items[0]);
+
+        if (fname == NULL) {
+            printf("Command not found: %s\n", tokens->items[0]);
+        }
+        else {
+            // tokens->items is already NULL-terminated,
+            // so it can be used as argv for execv()
+            execute(fname, tokens->items, tokens->size);
+
+            free(fname);
+        }
+
+
         // free memory
         free(input);
         free_tokens(tokens);
     }
+
+    return 0
 }
