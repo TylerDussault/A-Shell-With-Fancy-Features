2b029c0ce696bc0cad6fe9eca88b02fa4dc6c585
TylerDussault <tylerjdussault@gmail.com>
2026-09-23 16:23:50 -0400

    Fixed part 0 to include Ctrl-D

--- src/shell.c
+++ src/shell.c
@@ -7,21 +7,25 @@
 #include "env.h"
 #include "execute.h"
 #include "pathsearch.h"
-
+ 
 int main()
 {
     // REPL (read eval print loop)
     while (1) {
         // print prompt
         printf("%s@%s:%s>", getenv("USER"), getenv("MACHINE"), getenv("PWD"));
-
+ 
         // get input and tokenize
         char *input = get_input();
-        tokenlist *tokens = get_tokens(input, " ");
-
+        if (input == NULL) {   /* EOF: leave the loop like "exit" */
+            printf("\n");
+            break;
+        }
+        tokenlist *tokens = get_tokens(input, " \t");
+ 
         // tilde expand
         expand_tilde(tokens);
-
+ 
         // if empty input
         if (tokens->size == 0) {
             free(input);
@@ -31,7 +35,7 @@ int main()
         
         // find command in PATH
         char *fname = find_path(tokens->items[0]);
-
+ 
         if (fname == NULL) {
             printf("Command not found: %s\n", tokens->items[0]);
         }
@@ -39,15 +43,15 @@ int main()
             // tokens->items is already NULL-terminated,
             // so it can be used as argv for execv()
             execute(fname, tokens);
-
+ 
             free(fname);
         }
-
-
+ 
+ 
         // free memory
         free(input);
         free_tokens(tokens);
     }
-
+ 
     return 0;
 }
